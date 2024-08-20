df4cf5e0bad111e38f70c968a5327d2b4d774ed3
Jack Burton <jackeburton97@gmail.com>
2024-08-20 18:25:35 +0100

    adding submenu functionality

--- menu/src/App.tsx
+++ menu/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Menu from "./Menu";
-import { MenuItems } from "./types/menuTypes";
+import { MenuItems, SubMenuItem } from "./types/menuTypes";
 
 const menuItems: MenuItems = [
   {
@@ -36,6 +36,15 @@ function getSelectedIndex() {
   return menuItems.findIndex((item) => item.selected);
 }
 
+function getSelectedSubmenuIndex(selected_item_index: number) {
+  if (menuItems[selected_item_index].submenu) {
+    return menuItems[selected_item_index].submenu.findIndex(
+      (item: SubMenuItem) => item.selected
+    );
+  }
+  return -1;
+}
+
 function App() {
   const [menu, setMenu] = useState(menuItems);
 
@@ -49,10 +58,28 @@ function App() {
 
   const handleKeyPress = (e: { key: string }) => {
     const selected_item_index = getSelectedIndex();
+    const selected_submenu_item_index =
+      getSelectedSubmenuIndex(selected_item_index);
+    const is_submenu = selected_submenu_item_index !== -1;
     const updatedMenu = [...menu];
-    if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+    if (
+      e.key === "ArrowUp" ||
+      e.key === "ArrowDown" ||
+      e.key === "ArrowLeft" ||
+      e.key === "ArrowRight"
+    ) {
       if (e.key === "ArrowUp") {
-        if (selected_item_index !== 0) {
+        if (is_submenu) {
+          if (selected_submenu_item_index !== 0) {
+            // clear selected and set prev one as selected
+            updatedMenu[selected_item_index].submenu[
+              selected_submenu_item_index
+            ].selected = false;
+            updatedMenu[selected_item_index].submenu[
+              selected_submenu_item_index - 1
+            ].selected = true;
+          }
+        } else if (selected_item_index !== 0) {
           if (selected_item_index === -1) {
             // set to last index
             updatedMenu[updatedMenu.length].selected = true;
@@ -63,7 +90,20 @@ function App() {
           }
         }
       } else if (e.key === "ArrowDown") {
-        if (selected_item_index !== menuItems.length - 1) {
+        if (is_submenu) {
+          if (
+            selected_submenu_item_index !==
+            menuItems[selected_item_index].submenu.length - 1
+          ) {
+            // clear selected and set next one as selected
+            updatedMenu[selected_item_index].submenu[
+              selected_submenu_item_index
+            ].selected = false;
+            updatedMenu[selected_item_index].submenu[
+              selected_submenu_item_index + 1
+            ].selected = true;
+          }
+        } else if (selected_item_index !== menuItems.length - 1) {
           if (selected_item_index === -1) {
             // set to first index
             updatedMenu[0].selected = true;
@@ -72,6 +112,16 @@ function App() {
             updatedMenu[selected_item_index].selected = false;
             updatedMenu[selected_item_index + 1].selected = true;
           }
+        }
+      } else if (e.key === "ArrowRight") {
+        if (updatedMenu[selected_item_index].submenu) {
+          updatedMenu[selected_item_index].submenu[0].selected = true;
+        }
+      } else if (e.key === "ArrowLeft") {
+        if (updatedMenu[selected_item_index].submenu) {
+          updatedMenu[selected_item_index].submenu.map(
+            (item: SubMenuItem) => (item.selected = false)
+          );
         }
       }
       setMenu(updatedMenu);
